64c45f2f734bf5171f848f5ef39ba793f7b37dc0
BrendaNievesO <a01252478@itesm.mx>
2021-08-27 17:10:37 +0000

    quiz 2

--- assignments/01Convertidor/src/exercise.py
+++ assignments/01Convertidor/src/exercise.py
@@ -1,7 +1,30 @@
-# Escribe tus funciones abajo de esta línea
 
-def main():
     # Escribe tu código abajo de esta línea
+def pies_cm(pies):
+    total_cm =  pies*30.48
+    return total_cm
+def pulgadas_cm(pul):
+    total_cm =  pul*2.54
+    return total_cm
+def yar_cm(yar):
+    total_cm =  yar*91.44
+    return total_cm
+def main():
+    print('1. pies a cm, 2. pulgadas a cm, 3. yardas a cm')
+    op = int(input('Introduce una opcion: '))
+    cant= int(input('Introduce la cantidad: '))
+    if op ==1:
+        cm= pies_cm(cant)
+        print(cm)
+    elif op ==2:
+        cm= pulgadas_cm(cant)
+        print(cm)
+    elif op == 3:
+        cm = yardas_cm(cant)
+        print(cm)
+    else:
+        print('Error')
 
+            
 if __name__ == '__main__':
     main()
